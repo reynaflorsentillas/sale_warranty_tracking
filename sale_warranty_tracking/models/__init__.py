cbc9baa7a3d069151dc4b1f8dd787d0fd3a2f764
Reynaflor Sentillas <reynaflor.sentillas@omnitechnical.com>
2020-04-08 06:00:53 +0000

    Warranty Integration to Sales Order * Warranty Wizard model, fields, views * Set default values to wizard from sale order and sale order line * Add Warranty button in sale order line * Warranty smart button is sale order form * Added validation: check if warranty is allowed for the product (wizard and form) * Set default warranty period from product configuration on change of product * Added view and menu for: Expired Warranties * Added decoration to tree view of warranty. Red highlight for expired warranties, green highlight for in warranty

--- sale_warranty_tracking/models/__init__.py
+++ sale_warranty_tracking/models/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import sale_warranty
 from . import product
+from . import sale_order
--- sale_warranty_tracking/models/sale_warranty.py
+++ sale_warranty_tracking/models/sale_warranty.py
@@ -1,6 +1,8 @@
 from odoo import models, fields, api, _
 from dateutil import relativedelta
 
+from odoo.exceptions import UserError
+
 class SaleWarranty(models.Model):
     _name = 'sale.warranty'
     _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin', 'utm.mixin']
@@ -8,6 +10,7 @@ class SaleWarranty(models.Model):
 
     name = fields.Char(string='Warranty Reference', required=True, copy=False, readonly=True, states={'draft': [('readonly', False)]}, index=True, default=lambda self: _('New'))
     sale_id = fields.Many2one('sale.order', string='Sales Order', readonly=True, states={'draft': [('readonly', False)]})
+    sale_line_id = fields.Many2one('sale.order.line', string='Sale Order Line', readonly=True, states={'draft': [('readonly', False)]})
     partner_id = fields.Many2one('res.partner', string='Customer', required=True, readonly=True, states={'draft': [('readonly', False)]})
     product_id = fields.Many2one('product.product', string='Product', required=True, readonly=True, states={'draft': [('readonly', False)]})
     serial_no = fields.Char(string='Lot/Serial', readonly=True, states={'draft': [('readonly', False)]})
@@ -20,12 +23,12 @@ class SaleWarranty(models.Model):
         ('draft', 'Draft'),
         ('warranty', 'In Warranty'),
         ('expired', 'Expired'),
-    ], default='draft', string='Status', readonly=True, copy=False, index=True, tracking=3, compute='_compute_state')
-
-    @api.depends('purchase_date', 'expiry_date', 'warranty_period')
+    ], default='draft', string='Status', readonly=True, copy=False, index=True, tracking=3, compute='_compute_state', store=True)
+    
+    @api.depends('purchase_date', 'expiry_date', 'warranty_period', 'is_confirmed')
     def _compute_state(self):
         for record in self:
-            if record.is_confirmed == True and record.expiry_date == fields.Datetime.now().date():
+            if record.is_confirmed == True and record.expiry_date <= fields.Datetime.now().date():
                 record.state = 'expired'
             elif record.is_confirmed == True and record.expiry_date > fields.Datetime.now().date():
                 record.state = 'warranty'
@@ -46,6 +49,12 @@ class SaleWarranty(models.Model):
         warranty_period = self.warranty_period
         expiry_date = (purchase_date + relativedelta.relativedelta(months=warranty_period))
         self.expiry_date = expiry_date
+
+    @api.onchange('product_id')
+    def onchange_product_id(self):
+        # if self.product_id and self.product_id.is_allowWarranty != True:
+        #     raise UserError(_('Product is not configured for warranty!'))
+        self.warranty_period = self.product_id.warranty_period.days
 
     def action_confirm(self):
         for record in self:
